be7e000aba3cabe7bb7afa6588d7ec111f54161e
rbudde78 <robin.budde@gmail.com>
2022-01-28 11:12:34 -0600

    Added the rest of the logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,77 @@
 ﻿using System;
 
+Robot robot = new Robot();
+
+for (int index = 0; index < robot.Commands.Length; index++)
+{
+    string input = Console.ReadLine();
+
+    robot.Commands[index] = input switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SoutCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand()
+    };
+}
+
+Console.WriteLine();
+
+robot.Run();
+
+
+public class EastCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.Y++; }
+    }
+}
+public class WestCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.Y--; }
+    }
+}
+public class SoutCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.X--; }
+    }
+}
+public class NorthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.X++; }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
 public class Robot
 {
     public int X { get; set; }
@@ -8,16 +80,10 @@ public class Robot
     public RobotCommand[] Commands { get; } = new RobotCommand[3];
     public void Run()
     {
-        foreach(RobotCommand command in Commands)
+        foreach (RobotCommand command in Commands)
         {
             command.Run(this);
-            Console.WriteLine($"[{X}{Y}{IsPowered}");
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
         }
     }
-}
-
-public abstract class RobotCommand
-{
-    public abstract void Run(Robot robot);
-    
 }
