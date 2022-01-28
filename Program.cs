b0cf58d128f0f8810c75a55229bce9fb7a105515
rbudde78 <robin.budde@gmail.com>
2022-01-28 10:18:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand[] Commands { get; } = new RobotCommand[3];
+    public void Run()
+    {
+        foreach(RobotCommand command in Commands)
+        {
+            command.Run(this);
+            Console.WriteLine($"[{X}{Y}{IsPowered}");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public abstract void Run(Robot robot);
+    
+}
